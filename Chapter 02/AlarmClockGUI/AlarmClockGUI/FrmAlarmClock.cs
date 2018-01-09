039a8ef17d9372ecefab0276585f2fbfbd24f358
William M. <wmooney1984@gmail.com>
2018-01-09 00:39:52 -0500

    Chapter 2 - Alarm Clock GUI Exercise - Initial Commit

---
+++ Chapter 02/AlarmClockGUI/AlarmClockGUI/FrmAlarmClock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClockGUI
+{
+  public partial class FrmAlarmClock : Form
+  {
+    public FrmAlarmClock()
+    {
+      InitializeComponent();
+    }
+  }
+}
