887d4f8ed730fb4b09d7a993c10cbfabddb56f76
William M. <wmooney1984@gmail.com>
2018-01-09 01:00:19 -0500

    Chapter 2 - Alarm Clock GUI Exercise - Completed

--- Chapter 02/AlarmClockGUI/AlarmClockGUI/FrmAlarmClock.Designer.cs
+++ Chapter 02/AlarmClockGUI/AlarmClockGUI/FrmAlarmClock.Designer.cs
@@ -28,21 +28,167 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.button7 = new System.Windows.Forms.Button();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.label1 = new System.Windows.Forms.Label();
+      this.groupBox1.SuspendLayout();
       this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(17, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(72, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Hour";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(95, 12);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(72, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Minute";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(173, 12);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(72, 23);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "Second";
+      this.button3.UseVisualStyleBackColor = true;
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(251, 12);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(72, 23);
+      this.button4.TabIndex = 3;
+      this.button4.Text = "On";
+      this.button4.UseVisualStyleBackColor = true;
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(329, 12);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(72, 23);
+      this.button5.TabIndex = 4;
+      this.button5.Text = "Off";
+      this.button5.UseVisualStyleBackColor = true;
+      // 
+      // button6
+      // 
+      this.button6.Location = new System.Drawing.Point(329, 63);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(72, 23);
+      this.button6.TabIndex = 5;
+      this.button6.Text = "Timer";
+      this.button6.UseVisualStyleBackColor = true;
+      // 
+      // button7
+      // 
+      this.button7.Location = new System.Drawing.Point(17, 99);
+      this.button7.Name = "button7";
+      this.button7.Size = new System.Drawing.Size(384, 23);
+      this.button7.TabIndex = 6;
+      this.button7.Text = "SNOOZE";
+      this.button7.UseVisualStyleBackColor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Controls.Add(this.radioButton1);
+      this.groupBox1.Location = new System.Drawing.Point(155, 41);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(100, 50);
+      this.groupBox1.TabIndex = 8;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "AM/PM";
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Location = new System.Drawing.Point(6, 22);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(44, 19);
+      this.radioButton1.TabIndex = 0;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "AM";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(50, 22);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(43, 19);
+      this.radioButton2.TabIndex = 1;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "PM";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+      this.label1.Location = new System.Drawing.Point(17, 63);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(72, 23);
+      this.label1.TabIndex = 9;
+      this.label1.Text = "00:00:00";
       // 
       // FrmAlarmClock
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.ClientSize = new System.Drawing.Size(422, 131);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.button7);
+      this.Controls.Add(this.button6);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.Name = "FrmAlarmClock";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Alarm Clock";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Button button7;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.RadioButton radioButton2;
+    private System.Windows.Forms.RadioButton radioButton1;
+    private System.Windows.Forms.Label label1;
   }
 }
 
