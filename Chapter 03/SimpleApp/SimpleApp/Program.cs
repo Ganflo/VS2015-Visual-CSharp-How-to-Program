65bb1acc0277837b8bf44b8ad10264ac7fe01bcb
William M. <wmooney1984@gmail.com>
2018-01-09 17:48:47 -0500

    Chapter 3 - Simple App Example

---
+++ Chapter 03/SimpleApp/SimpleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApp
+{
+  class Welcome
+  {
+    // Main method begins execution of C# app
+    static void Main()
+    {
+      Console.WriteLine("Welcome to C# Programming");
+    }
+  }
+}
