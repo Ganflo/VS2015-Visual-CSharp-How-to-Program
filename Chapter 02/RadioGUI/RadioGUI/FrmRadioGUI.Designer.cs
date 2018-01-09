9d270b6dad010dd09b24ff28de40e9336b4732c1
William M. <wmooney1984@gmail.com>
2018-01-09 15:50:26 -0500

    Chapter 2 - Radio GUI Exercise - Completed

--- Chapter 02/RadioGUI/RadioGUI/FrmRadioGUI.Designer.cs
+++ Chapter 02/RadioGUI/RadioGUI/FrmRadioGUI.Designer.cs
@@ -28,21 +28,276 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRadioGUI));
+      this.grpStations = new System.Windows.Forms.GroupBox();
+      this.btn1 = new System.Windows.Forms.Button();
+      this.btn2 = new System.Windows.Forms.Button();
+      this.btn3 = new System.Windows.Forms.Button();
+      this.btn4 = new System.Windows.Forms.Button();
+      this.btn5 = new System.Windows.Forms.Button();
+      this.btn6 = new System.Windows.Forms.Button();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.chkRear = new System.Windows.Forms.CheckBox();
+      this.chkFront = new System.Windows.Forms.CheckBox();
+      this.btnPower = new System.Windows.Forms.Button();
+      this.grpVolume = new System.Windows.Forms.GroupBox();
+      this.grpTuning = new System.Windows.Forms.GroupBox();
+      this.chkMute = new System.Windows.Forms.CheckBox();
+      this.trkBar = new System.Windows.Forms.TrackBar();
+      this.label1 = new System.Windows.Forms.Label();
+      this.rdoAM = new System.Windows.Forms.RadioButton();
+      this.rdoPM = new System.Windows.Forms.RadioButton();
+      this.pic1 = new System.Windows.Forms.PictureBox();
+      this.grpStations.SuspendLayout();
+      this.groupBox1.SuspendLayout();
+      this.grpVolume.SuspendLayout();
+      this.grpTuning.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.trkBar)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
       this.SuspendLayout();
+      // 
+      // grpStations
+      // 
+      this.grpStations.Controls.Add(this.btn6);
+      this.grpStations.Controls.Add(this.btn5);
+      this.grpStations.Controls.Add(this.btn4);
+      this.grpStations.Controls.Add(this.btn3);
+      this.grpStations.Controls.Add(this.btn2);
+      this.grpStations.Controls.Add(this.btn1);
+      this.grpStations.Location = new System.Drawing.Point(12, 12);
+      this.grpStations.Name = "grpStations";
+      this.grpStations.Size = new System.Drawing.Size(180, 55);
+      this.grpStations.TabIndex = 0;
+      this.grpStations.TabStop = false;
+      this.grpStations.Text = "Pre-set Stations";
+      // 
+      // btn1
+      // 
+      this.btn1.Location = new System.Drawing.Point(6, 22);
+      this.btn1.Name = "btn1";
+      this.btn1.Size = new System.Drawing.Size(23, 23);
+      this.btn1.TabIndex = 0;
+      this.btn1.Text = "1";
+      this.btn1.UseVisualStyleBackColor = true;
+      // 
+      // btn2
+      // 
+      this.btn2.Location = new System.Drawing.Point(35, 22);
+      this.btn2.Name = "btn2";
+      this.btn2.Size = new System.Drawing.Size(23, 23);
+      this.btn2.TabIndex = 1;
+      this.btn2.Text = "2";
+      this.btn2.UseVisualStyleBackColor = true;
+      // 
+      // btn3
+      // 
+      this.btn3.Location = new System.Drawing.Point(64, 22);
+      this.btn3.Name = "btn3";
+      this.btn3.Size = new System.Drawing.Size(23, 23);
+      this.btn3.TabIndex = 2;
+      this.btn3.Text = "3";
+      this.btn3.UseVisualStyleBackColor = true;
+      // 
+      // btn4
+      // 
+      this.btn4.Location = new System.Drawing.Point(93, 22);
+      this.btn4.Name = "btn4";
+      this.btn4.Size = new System.Drawing.Size(23, 23);
+      this.btn4.TabIndex = 3;
+      this.btn4.Text = "4";
+      this.btn4.UseVisualStyleBackColor = true;
+      // 
+      // btn5
+      // 
+      this.btn5.Location = new System.Drawing.Point(122, 22);
+      this.btn5.Name = "btn5";
+      this.btn5.Size = new System.Drawing.Size(23, 23);
+      this.btn5.TabIndex = 4;
+      this.btn5.Text = "5";
+      this.btn5.UseVisualStyleBackColor = true;
+      // 
+      // btn6
+      // 
+      this.btn6.Location = new System.Drawing.Point(151, 22);
+      this.btn6.Name = "btn6";
+      this.btn6.Size = new System.Drawing.Size(23, 23);
+      this.btn6.TabIndex = 5;
+      this.btn6.Text = "6";
+      this.btn6.UseVisualStyleBackColor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.chkFront);
+      this.groupBox1.Controls.Add(this.chkRear);
+      this.groupBox1.Location = new System.Drawing.Point(198, 12);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(120, 55);
+      this.groupBox1.TabIndex = 6;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Speakers";
+      // 
+      // chkRear
+      // 
+      this.chkRear.AutoSize = true;
+      this.chkRear.Location = new System.Drawing.Point(10, 27);
+      this.chkRear.Name = "chkRear";
+      this.chkRear.Size = new System.Drawing.Size(49, 19);
+      this.chkRear.TabIndex = 0;
+      this.chkRear.Text = "Rear";
+      this.chkRear.UseVisualStyleBackColor = true;
+      // 
+      // chkFront
+      // 
+      this.chkFront.AutoSize = true;
+      this.chkFront.Location = new System.Drawing.Point(60, 26);
+      this.chkFront.Name = "chkFront";
+      this.chkFront.Size = new System.Drawing.Size(54, 19);
+      this.chkFront.TabIndex = 1;
+      this.chkFront.Text = "Front";
+      this.chkFront.UseVisualStyleBackColor = true;
+      // 
+      // btnPower
+      // 
+      this.btnPower.Location = new System.Drawing.Point(324, 12);
+      this.btnPower.Name = "btnPower";
+      this.btnPower.Size = new System.Drawing.Size(75, 55);
+      this.btnPower.TabIndex = 7;
+      this.btnPower.Text = "Power On/Off";
+      this.btnPower.UseVisualStyleBackColor = true;
+      // 
+      // grpVolume
+      // 
+      this.grpVolume.Controls.Add(this.trkBar);
+      this.grpVolume.Controls.Add(this.chkMute);
+      this.grpVolume.Location = new System.Drawing.Point(12, 73);
+      this.grpVolume.Name = "grpVolume";
+      this.grpVolume.Size = new System.Drawing.Size(180, 70);
+      this.grpVolume.TabIndex = 6;
+      this.grpVolume.TabStop = false;
+      this.grpVolume.Text = "Volume Control";
+      // 
+      // grpTuning
+      // 
+      this.grpTuning.Controls.Add(this.rdoPM);
+      this.grpTuning.Controls.Add(this.rdoAM);
+      this.grpTuning.Controls.Add(this.label1);
+      this.grpTuning.Location = new System.Drawing.Point(198, 73);
+      this.grpTuning.Name = "grpTuning";
+      this.grpTuning.Size = new System.Drawing.Size(120, 70);
+      this.grpTuning.TabIndex = 7;
+      this.grpTuning.TabStop = false;
+      this.grpTuning.Text = "Tuning";
+      // 
+      // chkMute
+      // 
+      this.chkMute.AutoSize = true;
+      this.chkMute.Location = new System.Drawing.Point(6, 22);
+      this.chkMute.Name = "chkMute";
+      this.chkMute.Size = new System.Drawing.Size(54, 19);
+      this.chkMute.TabIndex = 2;
+      this.chkMute.Text = "Mute";
+      this.chkMute.UseVisualStyleBackColor = true;
+      // 
+      // trkBar
+      // 
+      this.trkBar.Location = new System.Drawing.Point(64, 22);
+      this.trkBar.Name = "trkBar";
+      this.trkBar.Size = new System.Drawing.Size(101, 45);
+      this.trkBar.TabIndex = 3;
+      // 
+      // label1
+      // 
+      this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+      this.label1.Location = new System.Drawing.Point(6, 19);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(50, 44);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "92.9";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // rdoAM
+      // 
+      this.rdoAM.AutoSize = true;
+      this.rdoAM.Location = new System.Drawing.Point(60, 19);
+      this.rdoAM.Name = "rdoAM";
+      this.rdoAM.Size = new System.Drawing.Size(44, 19);
+      this.rdoAM.TabIndex = 1;
+      this.rdoAM.TabStop = true;
+      this.rdoAM.Text = "AM";
+      this.rdoAM.UseVisualStyleBackColor = true;
+      // 
+      // rdoPM
+      // 
+      this.rdoPM.AutoSize = true;
+      this.rdoPM.Location = new System.Drawing.Point(60, 44);
+      this.rdoPM.Name = "rdoPM";
+      this.rdoPM.Size = new System.Drawing.Size(43, 19);
+      this.rdoPM.TabIndex = 2;
+      this.rdoPM.TabStop = true;
+      this.rdoPM.Text = "PM";
+      this.rdoPM.UseVisualStyleBackColor = true;
+      // 
+      // pic1
+      // 
+      this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+      this.pic1.Location = new System.Drawing.Point(324, 73);
+      this.pic1.Name = "pic1";
+      this.pic1.Size = new System.Drawing.Size(75, 70);
+      this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pic1.TabIndex = 8;
+      this.pic1.TabStop = false;
       // 
       // FrmRadioGUI
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.ClientSize = new System.Drawing.Size(411, 155);
+      this.Controls.Add(this.pic1);
+      this.Controls.Add(this.grpTuning);
+      this.Controls.Add(this.grpVolume);
+      this.Controls.Add(this.btnPower);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.grpStations);
+      this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.Name = "FrmRadioGUI";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Radio";
+      this.grpStations.ResumeLayout(false);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.grpVolume.ResumeLayout(false);
+      this.grpVolume.PerformLayout();
+      this.grpTuning.ResumeLayout(false);
+      this.grpTuning.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.trkBar)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
       this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.GroupBox grpStations;
+    private System.Windows.Forms.Button btn6;
+    private System.Windows.Forms.Button btn5;
+    private System.Windows.Forms.Button btn4;
+    private System.Windows.Forms.Button btn3;
+    private System.Windows.Forms.Button btn2;
+    private System.Windows.Forms.Button btn1;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.CheckBox chkFront;
+    private System.Windows.Forms.CheckBox chkRear;
+    private System.Windows.Forms.Button btnPower;
+    private System.Windows.Forms.GroupBox grpVolume;
+    private System.Windows.Forms.TrackBar trkBar;
+    private System.Windows.Forms.CheckBox chkMute;
+    private System.Windows.Forms.GroupBox grpTuning;
+    private System.Windows.Forms.RadioButton rdoPM;
+    private System.Windows.Forms.RadioButton rdoAM;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.PictureBox pic1;
   }
 }
 
